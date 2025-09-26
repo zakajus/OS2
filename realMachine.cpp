3bf539399068057486c2f58ce03933e8742288d0
SkucaiteU <152654499+SkucaiteU@users.noreply.github.com>
2025-09-26 10:10:40 +0300

    Create realMachine.cpp

---
+++ realMachine.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+class realMachine
+{
+    private:
+        uint32 RAX;
+        uint32 RBX;
+        uint8 MODE;
+        uint16 DS;
+        uint16 ES;
+        uint16 PC;
+        uint16 TI;
+        uint8 DI;
+        uint8 SI;
+        uint8 SF;
+        uint32 PTR;
+    public:
+        realMachine(/* args */);
+        ~realMachine();
+};
