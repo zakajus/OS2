fc474d1118d1bf19daf9151759f33ea9d3e55e1f
SkucaiteU <152654499+SkucaiteU@users.noreply.github.com>
2025-09-26 10:13:04 +0300

    Create virtualMachine.cpp

---
+++ virtualMachine.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+class virtualMachine
+{
+    private:
+        uint32 RAX; //ar cia pointeriai ar ne
+        uint32 RBX;
+        uint16 DS;
+        uint16 ES;
+        uint16 PC;
+        uint8 SF;
+    public:
+        virtualMachine(/* args */);
+        ~virtualMachine();
+};
