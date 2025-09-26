7c93f2337b7dbe16ee00a49b8fba1c32e0d11e1e
SkucaiteU <152654499+SkucaiteU@users.noreply.github.com>
2025-09-26 10:13:36 +0300

    Update virtualMachine.cpp

--- virtualMachine.cpp
+++ virtualMachine.cpp
@@ -7,7 +7,7 @@ class virtualMachine
         uint32 RAX; //ar cia pointeriai ar ne
         uint32 RBX;
         uint16 DS;
-        uint16 ES;
+        uint16 CS;
         uint16 PC;
         uint8 SF;
     public:
