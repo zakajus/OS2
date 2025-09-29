596ee89dfe523e7c1f02a1402af30177c391fed7
SkucaiteU <152654499+SkucaiteU@users.noreply.github.com>
2025-09-29 12:26:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
+    //isvest kazka i ekrana (pasisveikinima:DD)
+    //Paklaust ar vykdyt programa is kart ar zingsniniu rezimu
 
+    //Zingsninis rezimas:
+    //Spausdinti registru reiksmes
+    //Spausdinti sekancia vykdoma komanda
+    //isoriniu irenginiu busenos                    kas cia?
+    //Vykdomos komandos VM puslapio reikšmės.
+
+    //po vykdomu komandu tikrinti SI, PI, TI reiksmes
+
+    //cia ir sukurt virtualias masinas
 
     
     return 0;
