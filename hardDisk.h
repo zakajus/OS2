406fe71d75dd7c4803cb5fafdd9b572ac42985c4
Kajus <mail@zakajus.com>
2025-10-13 01:43:23 +0300

    pridetas hdd atnaujinimas

--- hardDisk.h
+++ hardDisk.h
@@ -1,17 +1,35 @@
 #ifndef HARDDISK_H
 #define HARDDISK_H
 
+#define HDD_SIZE 3200
+
 #include <cstdint>
+#include <filesystem>
+
+using namespace std;
 
 class HardDisk {
 private:
-    uint32_t memory[3200];
+    uint32_t memory[HDD_SIZE]{};
+    static string fileName;
+    filesystem::file_time_type lastModificationTime;
 
 public:
     HardDisk();
+    explicit HardDisk(const string &fileName);
+
     ~HardDisk();
 
-    uint32_t getWord(uint16_t number) const;
+    /* Nuskaito visa faila tik jei jis buvo pakeistas, tai galima kviesti daznai
+     * cia aktualu, kad nereiketu perkrauti OS pakeitus hdd.txt */
+    void refreshMemory();
+
+    static filesystem::file_time_type getFileModificationTime() ;
+
+    // Iskvietimas gali atnaujinti lastModificationTime, tai jei reikia keliems palyginimams, reiksme issaugoti i kintamaji
+    bool fileWasModified();
+
+    uint32_t getWord(int number) const; // TODO: implement
 };
 
 #endif // HARDDISK_H
