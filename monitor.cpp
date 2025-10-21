20b49cd2a31cbfe0d2ac424ce5ca7672ea497190
SkucaiteU <152654499+SkucaiteU@users.noreply.github.com>
2025-10-21 12:09:31 +0300

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -22,22 +22,8 @@ void Monitor::displayText(uint32_t* src, uint32_t words){
             cout << '.'; // Non-printable characters
         }
     }
-
-    // for(uint32_t i = 0; i < words; i++){
-    //     uint8_t byte0 = (src[i] >> 24) & 0xFF;
-    //     uint8_t byte1 = (src[i] >> 16) & 0xFF;
-    //     uint8_t byte2 = (src[i] >> 8) & 0xFF;
-    //     uint8_t byte3 = src[i] & 0xFF;
-        
-    //     printf("%c", (byte0 >= 32 && byte0 <= 126) ? byte0 : '.');
-    //     printf("%c", (byte1 >= 32 && byte1 <= 126) ? byte1 : '.');
-    //     printf("%c", (byte2 >= 32 && byte2 <= 126) ? byte2 : '.');
-    //     printf("%c", (byte3 >= 32 && byte3 <= 126) ? byte3 : '.');
-    // }
-    printf("\n");
+    cout << endl;
 }
-
-
 
 void Monitor::display(uint32_t* src, uint32_t words, bool isNumber){
     if(isNumber){
